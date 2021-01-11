12f59bc84f118da347c73a35b644ace991e33a28
Chris <chris_pileggi@yahoo.com>
2021-01-10 22:27:43 -0500

    Initial Commit

---
+++ blog/entry.php
@@ -0,0 +1,36 @@
+<?php
+$title = "Chris Pileggi - Blog";
+include "../header.php";
+
+
+// PDO
+$pdo = new PDO("mysql:host=localhost;dbname=portfolio_main", 'root', '');
+
+$params = array(':slug' => $_GET['slug']);
+
+$sth = $pdo->prepare('
+    SELECT * FROM blog_entry
+    WHERE slug = :slug
+    ');
+
+$sth->execute($params);
+
+$result = $sth->fetchAll();
+
+
+?>
+
+<div class="contain">
+  <div class="head-contain"><h1><?php echo $result[0]['title'] ?></h1></div>
+
+    <div class="date-time-cont" ><time datetime="<?php echo date_create($result[0]["date"])->format('Y-m-d H:i:s') ?>"><span class="date-time"><?php echo date_create($result[0]["date"])->format('F d, Y'); ?></span></time></div>
+    <p class="blurb"><?php echo $result[0]['blurb'] ?></p>
+    <img src="../img/tree1.png" alt="" />
+    <div class="body-contain">
+      <?php echo $result[0]['body'] ?>
+</div>
+    </div>
+</div>
+
+
+<?php include "../footer.php";?>
