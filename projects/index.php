c5b761f63b6bf49696be0c06df9c25febf741ebb
cjpileggi <cjpileggi@gmail.com>
2021-01-11 22:15:05 -0500

    Updated row aligment CSS and PHP

--- projects/index.php
+++ projects/index.php
@@ -23,37 +23,23 @@ $rCnt = count($result);
 
 <div class="sect">
   <div class="container">
-  <?php
-  $rNum = 0;
-  for ($i = 1; $i <= floor($rCnt/3); $i++)
-  {?>
-  <div class="item-row">
-  <?php for ($j = 1; $j <= 3; $j++)
+
+  <div class="item-row item-row-last">
+  <?php for ($i = 0; $i < $rCnt; $i++)
   {?>
 
     <div class="item-item">
-      <a href="entry.php?slug=<?php echo $result[$rNum]['slug']; ?>">
-      <div class="item-img"><img src="../img/<?php echo $result[$rNum]['img']; ?>" alt="" /></div>
-      <h3><?php echo $result[$rNum]['name']; ?></h3>
-      <p><?php echo $result[$rNum]['blurb']; ?></p>
+      <a href="entry.php?slug=<?php echo $result[$i]['slug']; ?>">
+      <div class="item-img"><img src="../img/<?php echo $result[$i]['img']; ?>" alt="" /></div>
+      <h3><?php echo $result[$i]['name']; ?></h3>
+      <p><?php echo $result[$i]['blurb']; ?></p>
         </a>
     </div>
 
-<?php $rNum++; } }?>
-</div>
-<?php if ($rCnt % 3 > 0) { ?>
-<div class="item-row item-row-last">
-<?php for ($i = $rCnt - ($rCnt % 3); $i < $rCnt; $i++) { ?>
-  <div class="item-item">
-    <a href="entry.php?slug=<?php echo $result[$rNum]['slug']; ?>">
-    <div class="item-img"><img src="../img/<?php echo $result[$rNum]['img']; ?>" alt="" /></div>
-    <h3><?php echo $result[$rNum]['name']; ?></h3>
-    <p><?php echo $result[$rNum]['blurb']; ?></p>
-      </a>
-  </div>
-<?php $rNum++; } ?>
-</div>
 <?php } ?>
+</div>
+
+
 </div>
 </div>
 
