09285ae09e08cc3d53d08b8cdfdb6de0e1e6fa1a
Chris <chris_pileggi@yahoo.com>
2021-02-15 13:09:13 -0500

    GitHub link on project entry page

--- projects/entry.php
+++ projects/entry.php
@@ -14,6 +14,12 @@ WHERE slug = :slug", array(':slug' => $_GET['slug']));
   <p class="blurb"><?php echo $result[0]['blurb'] ?></p>
     <img src="../img/<?php echo $result[0]['img'] ?>" alt="" />
     <div class="body-contain">
+    <?php if($result[0]['github'] != "") { ?>
+      <div id="proj-git">
+        <div id="proj-git-ico"><a href="https://github.com/cjpileggi/<?= $result[0]['github'] ?>" target="_blank"><i class="fab fa-github"></i></a></div>
+        <a href="https://github.com/cjpileggi/<?= $result[0]['github'] ?>" target="_blank"><span>cjpileggi/<?= $result[0]['github'] ?></span></a>
+      </div>
+    <?php } ?>
       <?php echo $result[0]['body'] ?>
 </div>
     </div>
