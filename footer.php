01591547384212ff8674c3a0c30933e78d382360
cjpileggi <cjpileggi@gmail.com>
2021-10-31 17:07:55 -0400

    Update footer CSS property names

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-  <footer>
-    <div id="footer-items"><!--begin footer-->
+  <footer id="footer">
+    <div id="footer__inner"><!--begin footer-->
       <div id="cpy-rt">
         &#169; Copyright <?= date("Y"); ?> - Chris Pileggi
       </div>
       <div>
-        <ul id="footer-social-links">
+        <ul id="footer__social-links">
           <li><a href="https://github.com/cjpileggi" target="_blank"><i class="fab fa-github"></i></a></li>
           <li><a href="https://www.linkedin.com/in/pileggichristopher/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
           <li><a href="https://twitter.com/pileggiChris" target="_blank"><i class="fab fa-twitter"></i></a></li>
