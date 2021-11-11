4afff2ddfce469e220ff5be890c7fdb3ea6573a7
cjpileggi <cjpileggi@gmail.com>
2021-11-11 09:37:15 -0500

    Created 404 File

---
+++ err/404.php
@@ -0,0 +1,20 @@
+<?php
+/*
+* 404 Error Page
+*
+*
+*/
+$title = "404!";
+include "../header.php";
+$headOpac = true;
+include_once "../dbConfig.php";
+
+?>
+<div id="page-head" class="page-head--sub">
+  <div class="center">
+    <h1>404</h1>
+    <h2>Page not found!</h2>
+  </div>
+</div>
+
+<?php include "../footer.php";?>
