77cabc82f3417bf3f1c0cced370951f4b5cf13fd
Chris <chris_pileggi@yahoo.com>
2021-11-16 09:06:37 -0500

    Create icon folder constant path string and modify references

--- config.php
+++ config.php
@@ -9,4 +9,5 @@ define('DBPW', "");
 define("STYLES_PATH", "/css/");
 
 define("IMAGES_PATH", "/img/");
+define("ICONS_PATH", IMAGES_PATH . "icons/")
  ?>
--- header.php
+++ header.php
@@ -17,22 +17,22 @@
     <link rel="stylesheet" href="<?= STYLES_PATH . "prism.css"; ?>" /> <!-- PrismJS styles -->
 
     <!--favicon and shortcut icons-->
-    <link rel="apple-touch-icon" sizes="57x57" href="/img/icons/apple-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="60x60" href="/img/icons/apple-icon-60x60.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="/img/icons/apple-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="/img/icons/apple-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="/img/icons/apple-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="/img/icons/apple-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/img/icons/apple-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="/img/icons/apple-icon-152x152.png" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-icon-180x180.png" />
-    <link rel="icon" type="image/png" sizes="192x192"  href="/img/icons/android-icon-192x192.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="96x96" href="/img/icons/favicon-96x96.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/img/img/icons/favicon-16x16.png" />
-    <link rel="manifest" href="/img/icons/manifest.json" />
+    <link rel="apple-touch-icon" sizes="57x57" href=<?= ICONS_PATH . "apple-icon-57x57.png" ?> />
+    <link rel="apple-touch-icon" sizes="60x60" href=<?= ICONS_PATH . "apple-icon-60x60.png" ?> />
+    <link rel="apple-touch-icon" sizes="72x72" href=<?= ICONS_PATH . "apple-icon-72x72.png" ?> />
+    <link rel="apple-touch-icon" sizes="76x76" href=<?= ICONS_PATH . "apple-icon-76x76.png" ?> />
+    <link rel="apple-touch-icon" sizes="114x114" href=<?= ICONS_PATH . "apple-icon-114x114.png" ?> />
+    <link rel="apple-touch-icon" sizes="120x120" href=<?= ICONS_PATH ."apple-icon-120x120.png" ?> />
+    <link rel="apple-touch-icon" sizes="144x144" href=<?= ICONS_PATH . "apple-icon-144x144.png" ?> />
+    <link rel="apple-touch-icon" sizes="152x152" href=<?= ICONS_PATH . "apple-icon-152x152.png" ?> />
+    <link rel="apple-touch-icon" sizes="180x180" href=<?= ICONS_PATH . "apple-icon-180x180.png" ?> />
+    <link rel="icon" type="image/png" sizes="192x192"  href=<?= ICONS_PATH . "android-icon-192x192.png" ?> />
+    <link rel="icon" type="image/png" sizes="32x32" href=<?= ICONS_PATH . "favicon-32x32.png" ?> />
+    <link rel="icon" type="image/png" sizes="96x96" href=<?= ICONS_PATH . "favicon-96x96.png" ?> />
+    <link rel="icon" type="image/png" sizes="16x16" href=<?= ICONS_PATH . "favicon-16x16.png" ?> />
+    <link rel="manifest" href=<?= ICONS_PATH . "manifest.json" ?> />
     <meta name="msapplication-TileColor" content="#ffffff" />
-    <meta name="msapplication-TileImage" content="/img/icons/ms-icon-144x144.png" />
+    <meta name="msapplication-TileImage" content=<?= ICONS_PATH . "/ms-icon-144x144.png" ?> />
     <meta name="theme-color" content="#ffffff" />
 
     <!-- css3-mediaqueries.js for IE less than 9 -->
